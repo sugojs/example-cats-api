c8094fc6533c96067655d8b392eb1cf77f62da44
Francisco Javier Sucre González <fsucre@aluxion.com>
2019-06-25 15:35:56 +0200

    Integrated HelmetJS

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import { getMiddleware as parseJsonBody } from "@sugo/body-parser-json";
-import { createServer } from "@sugo/server";
+import { createServer, IHandler } from "@sugo/server";
 import { errorHandler, logRequest, logResponse } from "./middleware";
 import router from "./routes";
+import * as helmet from "helmet";
+// Compability fix made because the typings of helmetjs use the Express types, but
+// it is still compatible with NodeJS IncomingMessage and ServerResponse
+const helmetMiddleware: IHandler = (helmet() as unknown) as IHandler
 
 const server = createServer((req, res) => router.handle(req, res))
   .useMiddleware(errorHandler)
+  .useMiddleware(helmetMiddleware)
   .useMiddleware(parseJsonBody())
   .useMiddleware(logRequest)
   .useMiddleware(logResponse);
